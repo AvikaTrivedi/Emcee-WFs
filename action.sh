61624f368b3e96166a6ca75c8c48b2bf23af675e
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-07-24 19:07:27 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,4 +2,4 @@ branch=main
 git clone https://github.com/satyanandatripathi/soulmatemusic
 cd soulmatemusic
 docker build . -t daisyx
-docker run --privileged --env-file local.env --rm -i daisyx
+docker run --privileged --env-file .env --rm -i daisyx
